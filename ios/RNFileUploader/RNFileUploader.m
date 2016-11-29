716b84dabe2f629222e467898bebb36d21565bff
Tuan Dinh <leonacky@gmail.com>
2016-11-29 09:41:30 +0700

    init for ios

---
+++ ios/RNFileUploader/RNFileUploader.m
@@ -0,0 +1,13 @@
+//
+//  RNFileUploader.m
+//  RNFileUploader
+//
+//  Created by Tuan Dinh on 11/29/16.
+//  Copyright © 2016 Aotasoft. All rights reserved.
+//
+
+#import "RNFileUploader.h"
+
+@implementation RNFileUploader
+
+@end
